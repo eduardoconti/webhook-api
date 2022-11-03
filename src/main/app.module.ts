e47ab595a2211cf244e846abcccfa7f00c0fc2fd
Eduardo <eduardo.conti@gazin.com.br>
2022-11-03 10:08:29 -0300

    fix: remove bull

--- src/main/app.module.ts
+++ src/main/app.module.ts
@@ -1,23 +1,9 @@
-import { BullModule } from '@nestjs/bull';
 import { Module } from '@nestjs/common';
-import { ConfigModule, ConfigService } from '@nestjs/config';
 import { WebhookModule } from './webhook.module';
 import { AppController } from '@Presentation/controllers';
 import { Shutdown } from '@Infra/shutdown/shutdown';
 @Module({
-  imports: [
-    BullModule.forRootAsync({
-      imports: [ConfigModule],
-      useFactory: async (configService: ConfigService) => ({
-        redis: {
-          host: configService.get('REDIS_HOST'),
-          port: configService.get('REDIS_PORT'),
-        },
-      }),
-      inject: [ConfigService],
-    }),
-    WebhookModule,
-  ],
+  imports: [WebhookModule],
   controllers: [AppController],
   providers: [Shutdown],
 })
